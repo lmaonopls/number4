5634f20af3d711bff6ff271f388c59020daffade
alex khaduri <aleksandre.khaduri.1@btu.edu.ge>
2021-11-29 23:52:42 +0400

    first commit

---
+++ app/src/main/java/com/example/fragmentonni/fragments/FiltvebisGasatbobiFragment.kt
@@ -0,0 +1,7 @@
+package com.example.fragmentonni.fragments
+
+import androidx.fragment.app.Fragment
+import com.example.fragmentonni.R
+
+class FiltvebisGasatbobiFragment : Fragment (R.layout.fragment_filtvebis_gasatbobi) {
+}
---
+++ app/src/main/java/com/example/fragmentonni/fragments/NotificationsFragment.kt
@@ -0,0 +1,7 @@
+package com.example.fragmentonni.fragments
+
+import androidx.fragment.app.Fragment
+import com.example.fragmentonni.R
+
+class NotificationsFragment : Fragment(R.layout.fragment_notifications) {
+}
